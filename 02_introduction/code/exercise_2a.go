22be6dd2d6ca9c2cb2d9befc7e82e7016f319a18
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-10-01 19:19:57 -0400

    Add newline

--- 02_introduction/code/exercise_2a.go
+++ 02_introduction/code/exercise_2a.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World")
-	fmt.Printf("My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", "Noam", "New York City", 5, true)
+	fmt.Printf("My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t. \n", "Noam", "New York City", 5, true)
 }
