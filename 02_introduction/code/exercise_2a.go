02b32c0da53cbcce23eea9efb77c300d7a8e7f2c
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-10-01 19:18:05 -0400

    Add exercise 2a

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+	fmt.Printf("My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", "Noam", "New York City", 5, true)
+}
